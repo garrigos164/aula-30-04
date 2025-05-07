45c80dfe00470f3544f25a8e887bfe12d14bf55e
Olavo Garrigós <garrigos164@gmail.com>
2025-05-07 17:50:01 +0000

    Aula 07/05/2025

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const app = express()
+const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', './views');
@@ -9,9 +9,17 @@ app.use(express.static('public'));
 
 app.get('/', (resquest, response) => {
     //response.send('Testando!!');
-    response.render('home');
+    response.render('home', { nome: "Olavo" });
+});
+
+app.get('/tabuada', (request, response) => {
+    let resultado = [];
+    for (let cont = 1; cont <= 10; cont++){
+        resultado.push(cont * 5);
+        response.render('tabuada', { valores: resultado });
+    }
 });
 
 app.listen(3000, () => {
     console.log('Servidor na porta 3000');
-})
+});
